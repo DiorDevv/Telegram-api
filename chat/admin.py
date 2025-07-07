eb8dc35c90ca365fb9db528fcc4ec96e66253db5
Diyorbek <diyorbekaxmadjonov98@gamil.com>
2025-07-07 11:53:58 +0500

    Remove unused chat application models, views, and ASGI configuration

--- chat/admin.py
+++ chat/admin.py
@@ -1,28 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-
-from .models import User, Chat, Message
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_active', 'last_login')
-    search_fields = ('username', 'email')
-    list_filter = ('is_staff', 'is_active')
-    ordering = ('-last_login',)
-
-
-@admin.register(Chat)
-class ChatAdmin(admin.ModelAdmin):
-    list_display = ('id', 'is_group', 'created_at')
-    search_fields = ('id',)
-    ordering = ('-created_at',)
-
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'chat', 'sender', 'is_read', 'created_at')
-    search_fields = ('chat__id', 'sender__username', 'text')
-    list_filter = ('is_read', 'created_at')
-    ordering = ('-created_at',)
--- chat/views.py
+++ chat/views.py
@@ -1,21 +1,3 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from .models import Chat, Message
-from .serializers import ChatSerializer, MessageSerializer
+from django.shortcuts import render
 
-class ChatViewSet(viewsets.ModelViewSet):
-    serializer_class = ChatSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Chat.objects.filter(participants=self.request.user)
-
-class MessageViewSet(viewsets.ModelViewSet):
-    serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Message.objects.filter(chat__participants=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
+# Create your views here.
