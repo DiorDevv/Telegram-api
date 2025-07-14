e9a815df013f6fd8ee9b2d9463bf3cfea0667c76
Diyorbek <diyorbekaxmadjonov98@gamil.com>
2025-07-14 11:45:54 +0500

    Local changes before rebase

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -60,6 +61,7 @@ class MaqolaViewSet(ModelViewSet):
     queryset = Maqola.objects.all()
     serializer_class = MaqolaSerializer
     parser_classes = [MultiPartParser, FormParser]
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
